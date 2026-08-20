d5f75a48e0fd2a1f804260b5333e370a023c6619
DAN-MAN101 <dfegan11@gmail.com>
2026-08-20 14:53:27 +0000

    Add containerised project skeleton

---
+++ starter/src/main/java/com/neueda/leap/Main.java
@@ -0,0 +1,8 @@
+package com.neueda.leap;
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO: replace LeapVelocity with your team's actual name
+        System.out.println("Hello world from  LeapVelocity");
+    }
+}
