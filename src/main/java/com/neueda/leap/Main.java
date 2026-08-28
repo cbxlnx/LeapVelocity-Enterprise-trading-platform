ac75f38947866c204dcde2444325f215761f1f38
Kateryna Kozelko <150615889+cbxlnx@users.noreply.github.com>
2026-08-28 10:18:48 +0000

    Update greeting message in Main.java

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -2,7 +2,7 @@ package com.neueda.leap;
 
 public class Main {
     public static void main(String[] args) {
-         System.out.println("Hello world from the LeapVelocityfirm's Sprint 2 project skeleton");
+         System.out.println("Helloooooooo world from the LeapVelocityfirm's Sprint 2 project skeleton");
     }
 
     public static void greet() {
